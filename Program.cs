61f01f690e9df2bf23bc0befd9c95bf254d0a597
rdcsq <git@rdcsq.com>
2024-12-13 20:15:23 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using DiscordRPC;
+using DiscordRPC.Logging;
+
+const string leagueClientId = "401518684763586560";
+
+var quitEvent = new ManualResetEvent(false);
+
+if (args.Length == 0)
+{
+    Console.WriteLine("You have to provide the name of a champion as the first argument.");
+    return;
+}
+
+var client = new DiscordRpcClient(leagueClientId)
+{
+    Logger = new ConsoleLogger() { Level = LogLevel.Warning }
+};
+
+client.OnReady += (_, _) => { Console.WriteLine("Ready"); };
+
+client.Initialize();
+
+client.SetPresence(new RichPresence()
+{
+    Details = "Summoner's Rift",
+    State = "In Game",
+    Timestamps = new Timestamps()
+    {
+        Start = DateTime.Now.ToUniversalTime()
+            .AddSeconds(-new Random().Next(18000, 72000)) // between 5 and 20 hours
+    },
+    Assets = new Assets()
+    {
+        LargeImageKey = $"champ_{args[0]}"
+    }
+});
+
+Console.CancelKeyPress += (_, e) =>
+{
+    quitEvent.Set();
+    e.Cancel = true;
+};
+quitEvent.WaitOne();
---
+++ README.md
@@ -0,0 +1,7 @@
+# timeintherift
+
+A simple C# program to show as if you had been playing League of Legends for hours and hours on Discord.
+
+`dotnet run <champ_name>` (for example: `dotnet run neeko`)
+
+![Playing League of Legends for 19 hours as Neeko](./art/screenshot.webp)
